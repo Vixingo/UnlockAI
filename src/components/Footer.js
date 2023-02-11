3b133cd44cf4ac41a37ab3bc1fc0688b666045a3
Nazmul <Nazmul.abbu@gmail.com>
2023-02-12 03:21:53 +0600

    fool

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -30,82 +30,76 @@ function Footer() {
     return (
         <>
             <section className="Footer">
-                <Container maxWidth={"xl"}>
-                    <Grid
-                        spacing={2}
-                        container
-                        alignItems={"center"}
-                        sx={{
-                            borderBottom: "1px solid #464748",
-                            marginBottom: "10px",
-                            paddingBottom: "10px",
-                        }}
-                    >
-                        {" "}
-                        <Grid item xs={12} lg={4}>
-                            <Box textAlign={{ xs: "center", lg: "left" }}>
-                                <img
-                                    src="/img/logo.png"
-                                    alt=""
-                                    width={"240px"}
-                                />
-                            </Box>
-                        </Grid>
-                        <Grid item xs={12} lg={4}>
-                            <Box>
-                                <Stack
-                                    flexDirection={"row"}
-                                    justifyContent={"center"}
-                                >
-                                    {footerMenu.map((data) => {
-                                        return (
-                                            <Typography
-                                                sx={{
-                                                    paddingRight: "25px",
-                                                    color: "#fff",
-                                                }}
-                                            >
-                                                {data.title}
-                                            </Typography>
-                                        );
-                                    })}
-                                </Stack>
-                            </Box>
-                        </Grid>
-                        <Grid item xs={12} lg={4}>
-                            <Stack
-                                flexDirection={"row"}
-                                justifyContent={{
-                                    lg: "flex-end",
-                                    xs: "center",
-                                }}
-                            >
-                                <IconButton>
-                                    <FacebookOutlinedIcon />
-                                </IconButton>
-                                <IconButton>
-                                    <i class="fa-brands fa-twitter"></i>
-                                </IconButton>
-                                <IconButton>
-                                    <i class="fa-brands fa-instagram"></i>{" "}
-                                </IconButton>
-                                <IconButton>
-                                    <i class="fa-brands fa-linkedin"></i>{" "}
-                                </IconButton>
-                            </Stack>
-                        </Grid>
-                    </Grid>
-                    <Typography
-                        sx={{
-                            color: "#fff",
-                            textAlign: "center",
-                            fontSize: "14px",
-                            mb: 3,
-                        }}
-                    >
-                        © 2021 vizion All Rights Reserved.
-                    </Typography>
-                </Container>
+                <div className="container">
+                    <Box sx={{ margin: "20px" }}>
+                        <img src="/img/logo.png" alt="" width={"140px"} />
+                    </Box>
+
+                    <div className="row">
+                        <div className="col-md-3">
+                            <h2>Featured</h2>
+                            <ul>
+                                <li>Whisper</li>
+                                <li>Alignment</li>
+                                <li>Startup Fund</li>
+                            </ul>
+                        </div>
+                        <div className="col-md-3">
+                            <h2>API</h2>
+                            <ul>
+                                <li>Overview</li>
+                                <li>Pricing</li>
+                                <li>Examples</li>
+                                <li>Docs</li>
+                                <li>Terms & Policies</li>
+                                <li>Status</li>
+                                <li>Log in</li>
+                            </ul>
+                        </div>
+                        <div className="col-md-3">
+                            <h2>BLOG</h2>
+                            <ul>
+                                <li>Index</li>
+                                <li>Research</li>
+                                <li>Announcements</li>
+                                <li>Events</li>
+                                <li>Milestones</li>
+                            </ul>
+                        </div>
+                        <div className="col-md-3">
+                            <h2>Information</h2>
+                            <ul>
+                                <li>About Us</li>
+                                <li>Our Charter</li>
+                                <li>Our Research</li>
+                                <li>Publications</li>
+                                <li>Newsroom</li>
+                                <li>Careers</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="copy">
+                        <div className="footlink">
+                            <a href="#">UnlockAI © 2015-2023</a>{" "}
+                            <a href="#">Privacy Policy</a>{" "}
+                            <a href="#">Terms of Use</a>
+                        </div>
+                        <div className="icons">
+                            <a href="#">
+                                <i class="fa-brands fa-twitter"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fa-brands fa-youtube"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fa-brands fa-linkedin"></i>
+                            </a>
+                            <a href="#">
+                                <i class="fa-brands fa-github"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </section>
         </>
     );
